409b85416d8961191a110ea6534c403604a9ea28
Dogan Teke <dteke@actineo.de>
2022-02-28 15:46:23 +0100

    first commit

---
+++ public/build/assets/app.b4e3b84c.js
@@ -0,0 +1,1 @@
+import{_ as i,a,t as o}from"./vendor.73ab356f.js";window._=i;window.axios=a;window.axios.defaults.headers.common["X-Requested-With"]="XMLHttpRequest";o.initialize();
---
+++ resources/js/app.js
@@ -0,0 +1,5 @@
+import dynamicImportPolyfill from 'dynamic-import-polyfill'
+dynamicImportPolyfill.initialize()
+import '../css/app.css';
+import './bootstrap';
+
